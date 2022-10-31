364e8843052906aaeb9520bfef23a88a3a9a4382
VinayakBagaria <workvinayak95@gmail.com>
2022-11-01 02:45:37 +0530

    Refactor to table driven tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -6,21 +6,21 @@ import (
 )
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, want: 72.0},
+		{name: "Circle", shape: Circle{10}, want: math.Pi * math.Pow(10, 2)},
+	}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, math.Pi*math.Pow(10, 2))
-	})
-}
-
-func checkArea(t testing.TB, shape Shape, want float64) {
-	t.Helper()
-	got := shape.Area()
-	if got != want {
-		t.Errorf("got %g, wanted %g", got, want)
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g, wanted %g", got, tt.want)
+			}
+		})
 	}
 }
