fbb4444fd6bf2c7f95ce5f512041735dc9b549b3
VinayakBagaria <workvinayak95@gmail.com>
2022-11-03 02:56:00 +0530

    Add context cancellation in all child functions

--- contexts/contexts.go
+++ contexts/contexts.go
@@ -1,31 +1,22 @@
 package contexts
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
+	Fetch(ctx context.Context) (string, error)
 	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		// Race over 2 channels, 1 fetching data and impending cancellation of request
-		// so success or cancelled
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		data, err := store.Fetch(r.Context())
+		if err != nil {
+			return
 		}
+		fmt.Fprint(w, data)
 	}
 }
--- contexts/contexts_test.go
+++ contexts/contexts_test.go
@@ -2,6 +2,8 @@ package contexts
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,20 +15,58 @@ type StubStore struct {
 	cancelled bool
 }
 
-func (s *StubStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
+func (s *StubStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				log.Println("stub store go-routine got cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
 }
 
 func (s *StubStore) Cancel() {
 	s.cancelled = true
 }
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 func TestServer(t *testing.T) {
 	t.Run("tests normal server", func(t *testing.T) {
 		data := "hello"
-		stubStore := &StubStore{response: data}
-		svr := Server(stubStore)
+		svr := Server(&StubStore{response: data})
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
@@ -46,10 +86,10 @@ func TestServer(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 		svr.ServeHTTP(response, request)
 
-		if !stubStore.cancelled {
+		if response.written {
 			t.Error("store was not told to cancel")
 		}
 	})
