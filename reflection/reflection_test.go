a558517cef7cae473b4caa72ed05ff365f101e05
VinayakBagaria <workvinayak95@gmail.com>
2022-11-03 01:36:07 +0530

    Add support for map, array, slices, pointer, struct

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	cases := []struct {
 		name          string
@@ -14,17 +24,54 @@ func TestWalk(t *testing.T) {
 		{
 			"struct with one string field",
 			struct {
-				name string
+				Name string
 			}{"Chris"},
 			[]string{"Chris"},
 		},
 		{
 			"struct with two string fields",
 			struct {
-				name string
-				city string
+				Name string
+				City string
 			}{"Chris", "London"},
 			[]string{"Chris", "London"},
+		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"nested fields",
+			Person{"Chris", Profile{33, "London"}},
+			[]string{"Chris", "London"},
+		},
+		{
+			"pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
 		},
 	}
 
@@ -38,5 +85,31 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.expectedCalls)
 			}
 		})
+	}
+
+	t.Run("maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+		var got []string
+		Walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t testing.TB, got []string, want string) {
+	t.Helper()
+	contains := false
+	for _, x := range got {
+		if x == want {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", got, want)
 	}
 }
